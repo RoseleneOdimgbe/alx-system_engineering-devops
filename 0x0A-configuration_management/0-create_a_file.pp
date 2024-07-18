5330687185d5d30fb2ac14a8eb003f5e035a0a61
RoseleneOdimgbe <babymesa98@gmail.com>
2024-07-18 13:30:25 +0100

    0x0A

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# This creates a file
-file { '/tmp/school':
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content	=> 'I love Puppet'
-}
+# This manifest creates a file 'school' in 'tmp'
 
+file {'/tmp/school':
+  ensure  => present,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-#!/usr/bin/pup
-# This installs a specific version of flask (2.1.0)
+# This manifest installs Flask version 2.1.0
+
 package {'flask':
-	ensure	 => '2.1.0',
-	provider => 'pip'
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
